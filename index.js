f5a4d6fe37421231eeff3a402d46138a403dbce8
Jose pando <josepando@MacBookPro.lan>
2025-05-29 21:25:58 -0500

    finished

---
+++ index.js
@@ -0,0 +1,11 @@
+let code1;
+code1 = 7 + 3;
+let code2 = ( 100 - 20) / 2;
+let code3 = ( 234 % 39) + 39;
+let message = "The vault has been secured. the combination is:";
+let codeB = `${code1}-${code2}-${code3}`
+let codeA = code1 + "-" + code2 + "-" +code3
+console.log (message,codeA,codeB)
+console.log (codeA)
+
+
